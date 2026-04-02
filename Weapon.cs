b3a7b10cbc7536cc0a31691ba33fb0f7863b867f
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-02 12:09:05 +0300

    Implemented Weapon item

--- Weapon.cs
+++ Weapon.cs
@@ -1,5 +1,16 @@
 namespace Inventory {
 	public class Weapon : Item {
-		
+		public double Attack {get; }
+		public Weapon (string name, double weight, Rarity rarity, double attack) : base (name, weight, rarity) {
+			Attack = attack;
+		}
+
+		public override void Use(Hero hero) {
+			hero.Attack += Attack;
+		}
+
+		public override string GetInfo() {
+			return $"[{Rarity}] {Name} (weight: {Weight}, attack: +{Attack})";
+		}
 	}
 }
