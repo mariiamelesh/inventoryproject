09fe79011ae7a9db1fd9a2f61b924c4ef6f1ab20
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-02 12:24:19 +0300

    Moved enum outside the class

--- Item.cs
+++ Item.cs
@@ -1,19 +1,31 @@
 namespace Inventory {
-	public abstract class Item {
-		enum Rarity {
-			Common,
-			Rare, 
-			Epic, 
-			Legendary
-		}
+	public enum Rarity {
+		Common,
+		Rare, 
+		Epic, 
+		Legendary
+	}
+
+	public abstract class Item : IComparable<Item> {
 		
 		public string Name {get; }
 		public double Weight {get; }
-		public string Rarity {get; }
+		public Rarity Rarity {get; }
+		
+		public int CompareTo(Item other) {
+			if (other == null) return 1;
+			return this.Rarity.CompareTo(other.Rarity);
+        }
+
+		public Item(string name, double weight, Rarity rarity) {
+			Name = name;
+			Weight = weight;
+			Rarity = rarity;
+		}
 		
 		public abstract void Use(Hero hero);
-		public virtual void GetInfo() {
-			Console.WriteLine($"[{Rarity}] {Name} (weight: {Weight})");
+		public virtual string GetInfo() {
+			return $"[{Rarity}] {Name} (weight: {Weight})";
 		}
 	}
 }
