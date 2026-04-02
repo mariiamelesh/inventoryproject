c87aec7a326ee20df7c90bf9e7eab0d2b4513a69
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-02 12:11:34 +0300

    Implemented Potion item

--- Potion.cs
+++ Potion.cs
@@ -1,5 +1,17 @@
 namespace Inventory {
 	public class Potion : Item {
-		
+		public double Heal {get; }
+		public Potion (string name, double weight, Rarity rarity, double heal) : base (name, weight, rarity) {
+			Heal = heal;
+		}
+
+		public override void Use(Hero hero) {
+			hero.HP += Heal;
+			hero.Inventory.Remove(this);
+		}
+
+		public override string GetInfo() {
+			return $"[{Rarity}] {Name} (weight: {Weight}, heal: +{Heal})";
+		}
 	}
 }
