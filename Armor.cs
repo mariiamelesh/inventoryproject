7d8e34856bd7043e228cff508013fa1f0779fec8
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-02 12:17:30 +0300

    Implemented Armor item

--- Armor.cs
+++ Armor.cs
@@ -1,5 +1,16 @@
 namespace Inventory {
 	public class Armor : Item {
-		
+		public double Defense {get; }
+		public Armor (string name, double weight, Rarity rarity,double defense) : base (name, weight, rarity) {
+			Defense = defense;
+		}
+
+		public override void Use(Hero hero) {
+			hero.Defense += Defense;
+		}
+
+		public override string GetInfo() {
+			return $"[{Rarity}] {Name} (weight: {Weight}, defense: +{Defense})";
+		}
 	}
 }
