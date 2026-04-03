63efda5a8cc4d179cfcf6e5f2802a6803503aca8
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-03 09:48:56 +0300

    Added Hero property and constructor and main Run

--- ConsoleMenu.cs
+++ ConsoleMenu.cs
@@ -1,6 +1,15 @@
 namespace Inventory {
 	public class ConsoleMenu {
+		private Hero _hero;
+
+		public ConsoleMenu(Hero hero)
+		{
+			_hero = hero;
+		}
+
+		public void Run() {
+			
+		}
 		
 	}
-		
 }
