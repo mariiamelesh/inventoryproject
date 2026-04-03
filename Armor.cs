345bd20645989c7575918e3bcaab838bef2a0464
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-03 13:32:00 +0300

    Limited use amount to 1 time

--- Armor.cs
+++ Armor.cs
@@ -1,6 +1,7 @@
 namespace Inventory {
 	public class Armor : Item {
 		public double Defense {get; }
+		private int _useCount = 0;
 		public Armor (string name, double weight, Rarity rarity,double defense) : base (name, weight, rarity) {
 			Defense = defense;
 		}
@@ -8,7 +9,11 @@ namespace Inventory {
 		public override void Use(Hero hero) {
 			if (hero.Defense == 100) {
 				Console.WriteLine("Defense is full! Cannot use more armor");
-			} else {
+			} 
+			else if (_useCount == 1) {
+				Console.WriteLine("Cannot use armor more than once");
+			}
+			else {
 				hero.Defense += Defense;
 				Console.WriteLine($"Used {Name}! Defense now +{hero.Defense}");
 			}
--- Weapon.cs
+++ Weapon.cs
@@ -1,6 +1,7 @@
 namespace Inventory {
 	public class Weapon : Item {
 		public double Attack {get; }
+		private int _useCount = 0;
 		public Weapon (string name, double weight, Rarity rarity, double attack) : base (name, weight, rarity) {
 			Attack = attack;
 		}
@@ -8,7 +9,12 @@ namespace Inventory {
 		public override void Use(Hero hero) {
 			if (hero.Attack == 100) {
 				Console.WriteLine("Attack is full! Cannot use more weapon");
-			} else {
+			}
+			else if (_useCount == 1) {
+				Console.WriteLine("Cannot use weapon more than once");
+			}
+			else {
+				_useCount++;
 				hero.Attack += Attack;
 				Console.WriteLine($"Used {Name}! Attack now +{hero.Attack}");
 			}
