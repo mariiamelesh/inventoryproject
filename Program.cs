c56e0377e73e9e7e76136415f00270161835c5bd
skibidi <mariia.melesh.25@kse.org.ua>
2026-04-03 16:47:09 +0300

    Added tests to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 namespace Inventory {
 	public class Program {
 		static void Main(string[] args) {
-			
+			var artur = new Hero("Arthur");
+			var menu = new ConsoleMenu(artur);
+			menu.Run();
 		}
 	}
 }
